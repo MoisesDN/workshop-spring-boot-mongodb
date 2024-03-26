69339bdb95e0c9554c496ce91619553f42d2a08e
MoisesDN <m.dias009@gmail.com>
2024-03-26 17:40:54 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/moisesdias/ApiRestful_springboot_mongodb/domain/User.java
@@ -0,0 +1,62 @@
+package com.moisesdias.ApiRestful_springboot_mongodb.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class User implements Serializable {
+@Serial
+private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        return getId() != null ? getId().equals(user.getId()) : user.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
