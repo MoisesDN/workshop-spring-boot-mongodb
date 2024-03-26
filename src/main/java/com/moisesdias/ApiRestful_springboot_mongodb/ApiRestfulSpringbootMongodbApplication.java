9d8c203810262d51812882a9956d43b9910ff68a
MoisesDN <m.dias009@gmail.com>
2024-03-26 17:14:41 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/moisesdias/ApiRestful_springboot_mongodb/ApiRestfulSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.moisesdias.ApiRestful_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringbootMongodbApplication.class, args);
+	}
+
+}
