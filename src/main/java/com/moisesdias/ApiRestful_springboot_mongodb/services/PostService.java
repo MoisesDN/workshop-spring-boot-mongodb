6c0de35e93532a67a9a79efcc89aa8943d85ceb2
MoisesDN <m.dias009@gmail.com>
2024-04-04 16:22:13 -0300

    Obtendo um post por id

---
+++ src/main/java/com/moisesdias/ApiRestful_springboot_mongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.moisesdias.ApiRestful_springboot_mongodb.services;
+
+import com.moisesdias.ApiRestful_springboot_mongodb.domain.Post;
+import com.moisesdias.ApiRestful_springboot_mongodb.repository.PostRepository;
+import com.moisesdias.ApiRestful_springboot_mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
