40aa5422e1cd4a871b004a78ea6d6e994a37faf0
MoisesDN <m.dias009@gmail.com>
2024-04-05 16:07:27 -0300

    Consulta simples com query methods

--- src/main/java/com/moisesdias/ApiRestful_springboot_mongodb/resources/PostResource.java
+++ src/main/java/com/moisesdias/ApiRestful_springboot_mongodb/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.moisesdias.ApiRestful_springboot_mongodb.resources;
 
 import com.moisesdias.ApiRestful_springboot_mongodb.domain.Post;
+import com.moisesdias.ApiRestful_springboot_mongodb.resources.util.URL;
 import com.moisesdias.ApiRestful_springboot_mongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -16,11 +19,18 @@ public class PostResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
-
         Post obj = service.findById(id);
-
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 
 
 }
